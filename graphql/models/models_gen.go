4e83bccd591e1414ae48bef0e5f3fdbaafb23eb1
Vlad Melnichenko <vvmeln14@mts.ru>
2019-07-25 13:24:11 +0300

    roles and middles with logs

--- graphql/models/models_gen.go
+++ graphql/models/models_gen.go
@@ -2,6 +2,12 @@
 
 package models
 
+import (
+	"fmt"
+	"io"
+	"strconv"
+)
+
 type NewTodo struct {
 	Text   string `json:"text"`
 	UserID string `json:"userId"`
@@ -22,4 +28,45 @@ type User struct {
 	Name  string  `json:"name"`
 	Email string  `json:"email"`
 	Todos []*Todo `json:"todos"`
+}
+
+type Role string
+
+const (
+	RoleAdmin Role = "ADMIN"
+	RoleUser  Role = "USER"
+)
+
+var AllRole = []Role{
+	RoleAdmin,
+	RoleUser,
+}
+
+func (e Role) IsValid() bool {
+	switch e {
+	case RoleAdmin, RoleUser:
+		return true
+	}
+	return false
+}
+
+func (e Role) String() string {
+	return string(e)
+}
+
+func (e *Role) UnmarshalGQL(v interface{}) error {
+	str, ok := v.(string)
+	if !ok {
+		return fmt.Errorf("enums must be strings")
+	}
+
+	*e = Role(str)
+	if !e.IsValid() {
+		return fmt.Errorf("%s is not a valid Role", str)
+	}
+	return nil
+}
+
+func (e Role) MarshalGQL(w io.Writer) {
+	fmt.Fprint(w, strconv.Quote(e.String()))
 }
--- server/server.go
+++ server/server.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	graphql2 "github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/handler"
+	"github.com/go-chi/chi"
 
 	"github.com/asiman161/go-graphql/graphql"
 	"github.com/asiman161/go-graphql/graphql/dataloaders"
+	"github.com/asiman161/go-graphql/graphql/models"
 	"github.com/asiman161/go-graphql/localdb"
 )
 
@@ -20,20 +25,40 @@ func main() {
 		port = defaultPort
 	}
 
-	http.Handle("/", handler.Playground("GraphQL playground", "/query"))
-
+	r := chi.NewRouter()
+	r.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			auth := r.Header.Get("auth")
+			role := r.Header.Get("auth")
+			fmt.Printf("MIDDLEWARE[auth]: {auth: \"%s\", role: \"%s\"} \n", auth, role)
+			ctx := r.Context()
+			ctx = context.WithValue(ctx, "auth", auth)
+			ctx = context.WithValue(ctx, "role", role)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	})
 
 	db := &localdb.LocalDb{}
 
+
+	r.Handle("/", handler.Playground("GraphQL playground", "/query"))
+	config := graphql.NewRootResolvers(db)
+	config.Directives.HasRole = func(ctx context.Context, obj interface{}, next graphql2.Resolver, role models.Role) (res interface{}, err error) {
+		fmt.Printf("DIRECTIVE[role]: {auth: \"%s\", role: \"%s\"} \n", ctx.Value("auth"), ctx.Value("role"))
+		return next(ctx)
+	}
+
+	config.Directives.IsAuthenticated = func(ctx context.Context, obj interface{}, next graphql2.Resolver) (res interface{}, err error) {
+		fmt.Printf("DIRECTIVE[auth]: {auth: \"%s\", role: \"%s\"} \n", ctx.Value("auth"), ctx.Value("role"))
+		return next(ctx)
+	}
+
 	rootHandler := dataloaders.DataloaderMiddleware(db, handler.GraphQL(graphql.NewExecutableSchema(
-		graphql.NewRootResolvers(db),
+		config,
 	), handler.ComplexityLimit(250)))
 
-	//http.Handle("/query", handler.GraphQL(graphql.NewExecutableSchema(
-	//	graphql.NewRootResolvers(&gorm.DB{}),
-	//	), handler.ComplexityLimit(250)))
-	http.Handle("/query", rootHandler)
+	r.Handle("/query", rootHandler)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
